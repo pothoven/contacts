fe4cfe24e6532926e0105ade22c07aaca36a5a29
Steven Pothoven <steven@pothoven.net>
2011-08-31 14:11:00 -0400

    Initialize js variable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def google_javascript_include_tag
+    js = ""
     if ENV['RAILS_ENV'] == 'production'
       js += '<script src="' + request.protocol + 'ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js?ver=1.4.2" type="text/javascript"></script>'
       return js
