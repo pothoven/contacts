013a4f97aa25be89ad0c44b78ad27f8a24c09340
Steven Pothoven <steven@pothoven.net>
2011-08-31 14:40:31 -0400

    Added missing remove_item function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function remove_item(element) {
+	$(element).parentsUntil('.vcard_item').remove();
+}
+
+
