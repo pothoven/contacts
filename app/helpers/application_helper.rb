4ee93436de07c131c759c4a4a0aebeec545e3ebf
Steven Pothoven <steven@pothoven.net>
2011-09-08 19:47:25 -0400

    Migrated CSS, JS, and images to asset pipeline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 module ApplicationHelper
 
-  def google_javascript_include_tag
-    if ENV['RAILS_ENV'] == 'production'
-      js = raw('<script src="' + request.protocol + 'ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js?ver=1.6.2" type="text/javascript"></script>')
-    else
-      js = javascript_include_tag('jquery.min')
-    end
-    js += javascript_include_tag('jrails', 'application')
-    js += javascript_include_tag('jquery_ujs','jquery.jsocial')
-    return js
-  end
-
 end
